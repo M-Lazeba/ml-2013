3fa3d0f2acbd5f5f34e16a4be2f0ff4a4a552d20
M-Lazeba <m.lazeba@gmail.com>
2014-03-14 14:15:19 +0400

    Training start settings improved

--- maksim.lazeba/course-work-java/src/Main.java
+++ maksim.lazeba/course-work-java/src/Main.java
@@ -13,12 +13,19 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<TestCase> train = DatasetParser.getTrainSet(args[0], 4000);
-        List<TestCase> test = DatasetParser.getTestSet(args[0], 1000);
-        int threads = Math.min(Integer.valueOf(args[1]), 4);
+        int trainSize = System.getProperty("trainSet") == null ? 4000 : Integer.parseInt(System.getProperty("trainSet"));
+        int testSize = System.getProperty("testSet") == null ? 1000 : Integer.parseInt(System.getProperty("testSet"));
+        System.out.println("train: " + trainSize + " tests: " + testSize);
+        List<TestCase> train = DatasetParser.getTrainSet(args[0], trainSize);
+        List<TestCase> test = DatasetParser.getTestSet(args[0], testSize);
+        int threads = Math.min(Integer.valueOf(args[1]), 10);
         ExecutorService executor = Executors.newFixedThreadPool(threads);
+        String folder = System.getProperty("resFolder");
+        if (folder == null)
+            folder = "results";
+        System.out.println("Save results to " + folder);
         for (int i = 2; i < args.length; i++){
-            executor.submit(new Trainer(args[i], train, test, "results"));
+            executor.submit(new Trainer(args[i], train, test, folder));
         }
     }
 
